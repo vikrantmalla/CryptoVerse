e6b7322154e9ac0933d01030d388471d79cb4fb8
Vikrant Malla <vikrantmalla999@gmail.com>
2022-03-25 21:35:54 +0545

    implement Code-Splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from "react-router-dom";
 import "./app.css";
 import Layout from "./components/Layout";
-import HomePage from "./pages/HomePage";
-import CryptoCurrencies from "./pages/CryptoCurrencies";
-import CoinDetails from "./components/coinDetails/CoinDetails";
-import News from "./pages/News";
-import Watchlist from "./pages/Watchlist";
 import Alert from "./components/Alert";
-import { ThemeProvider, createTheme } from '@material-ui/core'
+import { ThemeProvider, createTheme, makeStyles } from '@material-ui/core';
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const CryptoCurrencies = React.lazy(() => import("./pages/CryptoCurrencies"));
+const CoinDetails = React.lazy(() => import("./components/coinDetails/CoinDetails"));
+const News = React.lazy(() => import("./pages/News"));
+const Watchlist = React.lazy(() => import("./pages/Watchlist"));
 const theme = createTheme({
   typography: {
     fontFamily: 'Montserrat',
@@ -17,19 +18,33 @@ const theme = createTheme({
     fontWeightBold: 700,
   }
 })
-
+const useStyles = makeStyles((theme) => {
+  return {
+    loading: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "80vh",
+      fontSize: "1.2rem",
+      fontWeight: "bold"
+    }
+  }
+})
 function App() {
+  const classes = useStyles()
   return (
     <ThemeProvider theme={theme}>
       <div className="app">
         <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/cryptocurrencies" element={<CryptoCurrencies />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/coin/:id" element={<CoinDetails />} />
-            <Route path="/watchlist" element={<Watchlist />} />
-          </Routes>
+          <Suspense fallback={<div className={classes.loading}>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/cryptocurrencies" element={<CryptoCurrencies />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/coin/:id" element={<CoinDetails />} />
+              <Route path="/watchlist" element={<Watchlist />} />
+            </Routes>
+          </Suspense>
           <Alert />
         </Layout>
       </div>
